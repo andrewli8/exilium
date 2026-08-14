3938fa2cb7274b8deee4de1f10d8345b6ac265e5
andrewli8 <lia210350@gmail.com>
2026-08-14 16:50:42 -0400

    chore: bump version to 0.2.7

--- src/cli.ts
+++ src/cli.ts
@@ -557,7 +557,7 @@ async function cmdLive(): Promise<void> {
       const ws = new WebSocket(buildLiveWsUrl(search), {
         headers: {
           Cookie: `POESESSID=${sessionId}`,
-          'User-Agent': 'Exilium/0.2.6 (+https://github.com/andrewli8/exilium)',
+          'User-Agent': 'Exilium/0.2.7 (+https://github.com/andrewli8/exilium)',
           Origin: 'https://www.pathofexile.com',
         },
       });
