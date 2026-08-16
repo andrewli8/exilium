0d91ef411e946dc6f885a0c84f028ce323760720
andrewli8 <lia210350@gmail.com>
2026-08-16 16:07:03 -0400

    chore: bump version to 0.2.14

--- src/trade/stash.ts
+++ src/trade/stash.ts
@@ -53,7 +53,7 @@ export async function fetchAllStashItems(
     const res = await scheduler.schedule('interactive', () => deps.fetchFn(buildStashUrl(accountName, league, tabIndex), {
         headers: {
           Cookie: `POESESSID=${sessionId}`,
-          'User-Agent': 'Exilium/0.2.13 (+https://github.com/andrewli8/exilium)',
+          'User-Agent': 'Exilium/0.2.14 (+https://github.com/andrewli8/exilium)',
         },
         ...(deps.signal === undefined ? {} : { signal: deps.signal }),
       }), deps.signal);
